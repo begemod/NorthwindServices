abb10e35ea6e362bf98417b0a90c02382e4ca70e
Roman Arekeev <roman_arekeev@epam.com>
2015-10-08 18:43:59 +0400

    Added WCF tests.

---
+++ Tests/WCFServiceTests/OrdersTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Tests.WCFServiceTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Tests.OrdersService;
+
+    [TestClass]
+    public class OrdersTests
+    {
+        [TestMethod]
+        public void GetAllMethodTest()
+        {
+            using (var client = new OrdersServiceClient())
+            {
+                client.GetAll();
+            }
+        }
+    }
+}
